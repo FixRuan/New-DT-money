62966e355ddcdb0c7f770bfee99cd4f2760dee8a
Ruan Pablo <ruan_goio10@hotmail.com>
2022-08-18 12:52:19 -0300

    Transaction type button

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -65,4 +65,32 @@ export const CloseButton = styled(Dialog.Close)`
   line-height: 0;
   cursor: pointer;
   color: ${({ theme }) => theme["gray-500"]};
-`
+`;
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`;
+
+interface TransactionTypeButtonProps{
+  variant: 'income' | 'outcome';
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  background-color: ${({ theme }) => theme["gray-700"]};
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  color: ${({ theme }) => theme["gray-100"]};
+
+  svg{
+   color: ${({theme, variant}) => theme[variant === 'income' ? 'green-500' : 'red-500']};
+  }
+`;
